fb66a0e84d3862500cbf55747bd9b69cdc3dbc8b
Thomas Rousiakis <rousiakisthom@gmail.com>
2022-08-10 20:11:43 +0300

    Set basic express app

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3001 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
