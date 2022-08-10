6915a553717ec5a6dcf599586f3af80dbb6cfa45
Thomas Rousiakis <rousiakisthom@gmail.com>
2022-08-10 20:30:30 +0300

    Set basic socket.io functionality

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Run when client connects
+
+io.on('connection', socket => {
+  //Welcome current user
+  socket.emit('message', 'Welcome to ChatApp!');
+
+  //Broadcast when user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  //Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+});
+
 const PORT = 3001 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
